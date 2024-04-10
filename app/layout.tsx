6fc5037d2de7872cd44632cd04145489d37ef08c
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-04-10 22:51:49 +0700

    Add Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-export default function Page() {
+const Page = () => {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -49,4 +49,6 @@ export default function Page() {
       </div>
     </main>
   );
-}
+};
+
+export default Page;
